5a4dd1e5675a36157aa7625d5e646bdee4693c68
Xein Effigy <>
2024-05-13 23:44:10 +0200

    read post data

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ source "https://rubygems.org"
 gemspec
 
 gem "async", "~> 2.11"
+gem 'async-io', "~> 1.11"
+
 gem "rake", "~> 13.0"
 
 gem "rubocop", "~> 1.21"
--- lib/dir_reader.rb
+++ lib/dir_reader.rb
@@ -1,7 +1,19 @@
+require "async"
+require "async/io"
+require "async/io/generic"
+require "async/io/protocol/line"
+require "yaml"
+
 class Section < Hash
   def initialize(path, type)
     super()
-    Dir.each_child(path) { |topic| self[topic] = type.new(File.join(path, topic)) }
+    Dir.each_child(path) do |topic|
+      dir = File.join(path, topic)
+      next unless File.directory?(dir)
+
+      self[topic] = type.new(dir)
+    end
+
     replace(
       sort_by { |topic, _| topic.rpartition(".@")[2] }
         .to_h
@@ -28,7 +40,47 @@ class Subtopic < Section
   end
 end
 
+class Threads < Section
+  def initialize(path)
+    super(path, ForumThread)
+  end
+end
+
 class Post
+  attr_reader :meta, :content, :threads
+
+  def initialize(path)
+    @threads = Threads.new(path)
+    Async do
+      File.open(File.join(path, "latest.md"), "r") do |file|
+        generic = Async::IO::Stream.new(file)
+        lines = Async::IO::Protocol::Line.new(generic).each_line
+        lines.next
+        meta = lines.take_while { |line| line.strip != "---" }.join("\n")
+        # lines.next while lines.peek.strip.empty?
+        @meta = YAML.load(meta)
+        @content = generic.read
+      end
+    end
+  end
+
+  def to_json(*args)
+    json = @meta
+    json.merge!(
+      'content' => @content,
+      'threads' => @threads
+    )
+    json.to_json(*args)
+  end
+end
+
+class ForumThread < Section
+  def initialize(path)
+    super(path, Comment)
+  end
+end
+
+class Comment
   def initialize(_)
   end
 end
