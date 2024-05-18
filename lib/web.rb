274450a1f895e58cbec50c848f273ce62b6f80c8
Xein Effigy <>
2024-05-18 18:37:17 +0200

    fix post selection

--- lib/web.rb
+++ lib/web.rb
@@ -57,16 +57,16 @@ module FaunWeb
 
     get "/topics/:id/" do |id|
       topic = @forum.topic(id.to_i)
-      slim :details, :locals => { :posts => topic.posts, :active => active_from(:post) }
+      slim :details, :locals => { :posts => topic.posts, :active => active_from(:post), :topic => "#{id}" }
     end
 
     get "/topics/:id/:subid/" do |id, subid|
       sub = @forum.subtopic(id.to_i, subid.to_i)
-      slim :details, :locals => { :posts => sub.posts, :active => active_from(:post) }
+      slim :details, :locals => { :posts => sub.posts, :active => active_from(:post), :topic => "#{id}.#{subid}" }
     end
 
     get "/posts/" do
-      slim :details, :locals => { :posts => @forum.posts, :active => active_from(:post) }
+      slim :details, :locals => { :posts => @forum.posts, :active => active_from(:post), :topic => nil }
     end
 
     get "/posts/:id/" do |id|
